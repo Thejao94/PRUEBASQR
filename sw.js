f6fff77859ed6701150c58d2246912f4ec05f8ac
Thejao94 <114195847+Thejao94@users.noreply.github.com>
2025-10-03 17:59:52 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,13 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("qr-cache").then(cache => {
-      return cache.addAll(["index.html", "manifest.json", "icon-192.png", "icon-512.png"]);
+      return cache.addAll([
+        "index.html",
+        "manifest.json",
+        "icon-192.png",
+        "icon-512.png",
+        "html5-qrcode.min.js"
+      ]);
     })
   );
 });
